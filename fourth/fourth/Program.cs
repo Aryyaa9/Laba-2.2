2e6735323b8888e0acec6bc243cea7d0fea00c0f
Aryyaa9 <153473157+Aryyaa9@users.noreply.github.com>
2024-04-03 12:52:15 +0700

    Add files via upload

---
+++ fourth/fourth/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class Counter
+{
+    int value; 
+    public Counter()
+    {
+        value = 0; 
+    }
+    public Counter(int startValue)
+    {
+        value = startValue;
+    }
+    
+    public void Increment()
+    {
+        value++;
+    }
+    
+    public void Decrement()
+    {
+        value--;
+    }
+    
+    public int Value
+    {
+        get { return value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Counter counter1 = new Counter();
+        
+        counter1.Increment();
+        Console.WriteLine("Значение счетчика 1 после увеличения: " + counter1.Value);
+        
+        Counter counter2 = new Counter(10);
+        
+        counter2.Decrement();
+        Console.WriteLine("Значение счетчика 2 после уменьшения: " + counter2.Value);
+    }
+}
